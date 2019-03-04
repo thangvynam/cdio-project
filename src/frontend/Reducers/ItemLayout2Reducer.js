48f6508305b69ccdac8d28bc0b719ac83933d91b
Phuc <phuctnh@vng.com.vn>
2019-03-04 21:48:10 +0700

    add table layout2

--- src/frontend/Reducers/ItemLayout2Reducer.js
+++ src/frontend/Reducers/ItemLayout2Reducer.js
@@ -1,14 +1,19 @@
-import {ADD_DATA_LAYOUT_2} from '../Constant/ActionType';
+import {ADD_DATA_LAYOUT_2, SAVE_DATA_LAYOUT_2} from '../Constant/ActionType';
 
 const itemLayout2InitialState = {
-    description: ''
+    previewInfo: ''
 }
 const ItemLayout2Reducer = (state = itemLayout2InitialState, action) => {
     switch (action.type) {
         case ADD_DATA_LAYOUT_2:
             return {
                 ...state,
-                description: action.description
+                previewInfo: action.description
+            }
+        case SAVE_DATA_LAYOUT_2:
+            return {
+                ...state, 
+                previewInfo: action.data[0].description
             }
         default:
             return state

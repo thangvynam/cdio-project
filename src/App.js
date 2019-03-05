448a19fe511d68d294eb4389e91cb6b602b550b5
LAP60123 <namtv3@vng.com.vn>
2019-03-05 13:28:45 +0700

    add dynamic import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
-import Dashboard from './frontend/components/decuongmonhoc/index/index'; 
-import Index from './frontend/Index/Index';
-                          
+const DynamicImport = (LoaderComponent) => Loadable({
+  loader: LoaderComponent,
+  loading: () => null
+});
+
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/de-cuong-mon-hoc/:type' component={Dashboard} />
-        <Route exact path='/' component={Index} />
+        {/* Dashboard */}
+        <Route exact path='/de-cuong-mon-hoc/:type' component={DynamicImport(() => import('./frontend/components/decuongmonhoc/index/index'))} />
+        {/* Index */}
+        <Route exact path='/' component={DynamicImport(() => import('./frontend/Index/Index'))} />
       </Switch>
     );
   }

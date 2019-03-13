284b070dc67497b7039b838e32338a54845d5229
BARCELONA\user <hoangphuct97@gmail.com>
2019-03-13 22:08:20 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,18 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Dashboard from "./frontend/components/decuongmonhoc/index/index";
-<<<<<<< HEAD
-import Index from "./frontend/Index/Index";
-import Page404 from "./frontend/NotFound/Page404";
-=======
 // import Index from "./frontend/Index/Index";
 import Home from "./frontend/components/trangchu/index";
->>>>>>> master
+import Page404 from "./frontend/NotFound/Page404";
 class App extends Component {
   render() {
     return (
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/de-cuong-mon-hoc/:type" component={Dashboard} />
-        <Route exact path="/" component={Index} />
-        <Route component={Page404} />
-=======
         <Route exact path="/:type1/:monhoc/:type" component={Dashboard} />
         <Route exact path="/" component={Home} />
         <Route exact path="/de-cuong-mon-hoc" component={Home} />
         <Route exact path="/tab-2" component={Home} />
->>>>>>> master
+        <Route component={Page404}/>
       </Switch>
     );
   }

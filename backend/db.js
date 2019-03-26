e8de02effc5159bdf90142a02161ed55a460bc4e
User-PC\User <hungphat11997@gmail.com>
2019-03-26 23:36:33 +0700

    fix reload matrix

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '123456',
+  password : 'mug3nnsx',
   database : 'syllabusdb'
 });
 connection.connect();

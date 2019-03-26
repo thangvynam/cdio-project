8b0489b0e0d555425ffa93b64b7a799c4143e34e
Phu <ldphu410@gmail.com>
2019-03-26 20:02:02 +0700

    refactor layout1

--- backend/models/ThongTinChungModel.js
+++ backend/models/ThongTinChungModel.js
@@ -1,21 +1,22 @@
 var sql = require('../db');
 
-const collect = (result) => {
-    sql.query("SELECT * FROM thong_tin_chung", (err, res) => {
-        if (err) {
-            console.log("error:", err);
-            result(null, err)
-        } else {
-            result(null, res);
-        }
-    })
+const collect = (id, result) => {
+    console.log(id.id)
+    sql.query(`select * from thong_tin_chung where id = ${id.id} and del_flag = 0`,
+        (err, res) => {
+            if (err) {
+                result(err);
+            }
+            else {
+                result(res[0]);
+            }
+        })
 }
 
-const add = (desc, result) => {
-    //sql.query(`update into thong_tin_chung(ten_mon_hoc_tv, ten_mon_hoc_ta, ma_so, khoi_kien_thuc, so_tin_chi, so_tiet_ly_thuyet, so_tiet_thuc_hanh, so_tiet_tu_hoc, cac_mon_hoc_tien_quyet) values ('${desc.ten_mon_hoc_tv}', '${desc.ten_mon_hoc_ta}', '${desc.ma_so}', '${desc.khoi_kien_thuc}', ${desc.so_tin_chi}, ${desc.so_tiet_ly_thuyet}, ${desc.so_tiet_thuc_hanh}, ${desc.so_tiet_tu_hoc}, '${desc.cac_mon_hoc_tien_quyet}')`, 
-    //sql.query(`update into thong_tin_chung(ten_mon_hoc_tv, ten_mon_hoc_ta, ma_so, khoi_kien_thuc, so_tin_chi, so_tiet_ly_thuyet, so_tiet_thuc_hanh, so_tiet_tu_hoc, cac_mon_hoc_tien_quyet) values ('${desc.ten_mon_hoc_tv}', '${desc.ten_mon_hoc_ta}', '${desc.ma_so}', '${desc.khoi_kien_thuc}', ${desc.so_tin_chi}, ${desc.so_tiet_ly_thuyet}, ${desc.so_tiet_thuc_hanh}, ${desc.so_tiet_tu_hoc}, '${desc.cac_mon_hoc_tien_quyet}')`, 
-    let sql1 = `UPDATE thong_tin_chung SET ten_mon_hoc_tv=?, ten_mon_hoc_ta=?, ma_so=?, khoi_kien_thuc=?, so_tin_chi=?, so_tiet_ly_thuyet=?, so_tiet_thuc_hanh=?, so_tiet_tu_hoc=?, cac_mon_hoc_tien_quyet = ? WHERE id = ?`;
-    let data = [desc.ten_mon_hoc_tv, desc.ten_mon_hoc_ta, desc.ma_so, desc.khoi_kien_thuc, desc.so_tin_chi, desc.so_tiet_ly_thuyet, desc.so_tiet_thuc_hanh, desc.so_tiet_tu_hoc, desc.cac_mon_hoc_tien_quyet, 1];
+const add = (id, desc, result) => {
+    console.log(id.id);
+    let sql1 = `UPDATE thong_tin_chung SET ten_mon_hoc_tv=?, ten_mon_hoc_ta=?, ma_so=?, khoi_kien_thuc=?, so_tin_chi=?, so_tiet_ly_thuyet=?, so_tiet_thuc_hanh=?, so_tiet_tu_hoc=?, cac_mon_hoc_tien_quyet = ? WHERE id = ? and del_flag = 0`;
+    let data = [desc.tenMonHocTV, desc.tenMonHocTA, desc.maMonHoc, desc.khoiKienThuc, desc.soTinChi, desc.tietLyThuyet, desc.tietThucHanh, desc.tietTuHoc, desc.monTienQuyet, id.id];
     sql.query(sql1, data, (err, res) => {
         if (err) {
             console.log("error:", err);

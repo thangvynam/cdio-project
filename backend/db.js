4c97bcbff781b4457e2e67527213ffa7519830e7
BARCELONA\user <hoangphuct97@gmail.com>
2019-03-30 21:57:46 +0700

    add auto update log

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'mug3nnsx',
+  password : 'rootroot',
   database : 'syllabusdb'
 });
 connection.connect();

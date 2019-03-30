2b1a12918eb8a391356d05fd40ffa69fd46adff0
Phuc. Ngo Nguyen Hong <phucnnh@vng.com.vn>
2019-03-30 16:56:52 +0700

    edit layout6

--- backend/models/Model6.js
+++ backend/models/Model6.js
@@ -3,10 +3,19 @@ var sql = require('../db');
 var Model6 = (data) => {
     this.data = data;
 }
-Model6.add = (data, result) => {
-    data.forEach((item,index)=> {
+Model6.add = (body, result) => {
 
-        sql.query(`insert into ke_hoach_thuc_hanh(tuan,chu_de,thong_tin_chung_id) values ('${item.week}','${item.titleName}',${item.thong_tin_chung_id})`, 
+  sql.query(`UPDATE ke_hoach_thuc_hanh SET del_flag = 1 WHERE thong_tin_chung_id = ${body.thong_tin_chung_id}`,(err,res)=>{
+    if(err){
+      console.log("err: ",err);
+      return result(err,null);
+    }
+  })
+
+
+    body.data.forEach((item,index)=> {
+
+        sql.query(`insert into ke_hoach_thuc_hanh(tuan,chu_de,thong_tin_chung_id) values ('${item.week}','${item.titleName}',${body.thong_tin_chung_id})`, 
         (err, res) => {
           if (err) {
               console.log("error:", err);
@@ -15,7 +24,6 @@ Model6.add = (data, result) => {
               let id = res.insertId;
 
               item.teachingActs.forEach((item,index)=>{
-                  console.log("item: ", item);
                 sql.query(`insert into khth_has_hdd(ke_hoach_thuc_hanh_id,hoat_dong_day_id) values (${id},${item})`, 
                 (err, res) => {
                   if (err) {
@@ -28,7 +36,6 @@ Model6.add = (data, result) => {
               })
 
               item.standardOutput.forEach((item,index)=>{
-                console.log("item: ", item);
               sql.query(`insert into khth_has_cdrmh(ke_hoach_thuc_hanh_id,chuan_dau_ra_mon_hoc_id) values (${id},${item})`, 
               (err, res) => {
                 if (err) {
@@ -40,7 +47,6 @@ Model6.add = (data, result) => {
             })
 
             item.evalActs.forEach((item,index)=>{
-                console.log("item: ", item);
               sql.query(`insert into khth_has_dg(ke_hoach_thuc_hanh_id,danh_gia_id) values (${id},${item})`, 
               (err, res) => {
                 if (err) {
@@ -52,9 +58,6 @@ Model6.add = (data, result) => {
             })
 
             return result(null,res);
-
-
-
             
           }
       })
@@ -64,8 +67,8 @@ Model6.add = (data, result) => {
 }
 
 
-Model6.getTeachingArts = (result)=>{
-  sql.query(`select * from hoat_dong_day`,(err,res)=>{
+Model6.getTeachingActs = (result)=>{
+  sql.query(`select * from hoat_dong_day where danh_muc = 1`,(err,res)=>{
     if(err){
       console.log("err: ",err);
       return result(err,null);
@@ -73,10 +76,59 @@ Model6.getTeachingArts = (result)=>{
     else
     //console.log("result: ",res);
     return result(null,res);
-  });
-
-
-
+  })
 }
+
+Model6.getEvalActs = (idSubject,result)=>{
+    sql.query(`SELECT id,ma FROM danh_gia WHERE thong_tin_chung_id = ${idSubject} AND del_flag = 0`,(err,res)=>{
+      if(err){
+        console.log("err: ",err);
+        return result(err,null);
+      }
+      else
+      return result(null,res);
+    });
+  }
+  
+  Model6.getStandardOutput = (idSubject,result)=>{
+    sql.query(`SELECT id,muc_tieu FROM muc_tieu_mon_hoc WHERE thong_tin_chung_id = ${idSubject} AND del_flag = 0`,(err,listMT)=>{
+      if(err){
+        console.log("err: ",err);
+        return result(err,null);
+      }
+      else{
+        let standardOutput = [];
+        listMT.forEach((muctieu,index)=>{
+          sql.query(`SELECT id,chuan_dau_ra FROM chuan_dau_ra_mon_hoc
+          WHERE muc_tieu_mon_hoc_id = ${muctieu.id} AND del_flag = 0`,(err,listCdr)=>{
+            if(err){
+              console.log("err: ",err);
+              return result(err,null);
+            }
+            let temp = {
+              "muc_tieu": muctieu.muc_tieu,
+              "cdr":listCdr,
+            }
+            standardOutput.push(temp);
+            if (index === listMT.length - 1) return result(null,standardOutput);
+
+          })
+
+        })
+      
+
+
+      }
+    
+
+
+
+      
+    });
+  }
+
+
+
+
 
 module.exports = Model6;

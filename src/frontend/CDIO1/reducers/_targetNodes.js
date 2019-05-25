fdaf3d0dbcf972af7535daea22d58ab068755756
MockingBird 11235 <mocking.key118@gmail.com>
2019-05-25 13:20:19 +0700

    code reducers of cdio1

--- src/frontend/CDIO1/reducers/_targetNodes.js
+++ src/frontend/CDIO1/reducers/_targetNodes.js
@@ -6,8 +6,6 @@ export const targetNodes = (state = [], action) => {
       return action.targetNodes;
     case types.LOAD_TARGET_EDUPROGRAM_ERROR:
       return [];
-    case types.SAVE_TARGET_EDUPROGRAM_ERROR:
-      return action.targetNodes;
     default:
       return state;
   }

95ad5038b115cf3086a792f3e978b0f8fbecee71
Phuc <phuctnh@vng.com.vn>
2019-05-26 11:59:45 +0700

    Add trycatch

--- src/frontend/Log/LogForm.js
+++ src/frontend/Log/LogForm.js
@@ -100,10 +100,13 @@ class LogForm extends Component {
         if(this.props.logReducer.renderComment || !this.props.logReducer.renderComment){
             
             if(data) {
-            
-                data.sort((a, b) => {
-                    return b.thoi_gian - a.thoi_gian
-                })
+                try {
+                    data.sort((a, b) => {
+                        return b.thoi_gian - a.thoi_gian
+                    })
+                } catch (error) {
+                    return error
+                } 
                 LogComment = data.map((itemparent, ich) => {
                     let con = this.props.logReducer.logComment2.map((itemchilren, ic) => {
                         if (itemchilren.log_id === itemparent.id) {

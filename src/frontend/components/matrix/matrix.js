b51c0d5c0bbbf24de279d907d96d3ee2e43379ef
User-PC\User <hungphat11997@gmail.com>
2019-06-03 15:22:50 +0700

    add

--- src/frontend/components/matrix/matrix.js
+++ src/frontend/components/matrix/matrix.js
@@ -398,7 +398,7 @@ class Matrix extends Component {
 
     cloneEditMatrix = ()=>{
 
-        $.insertStandardMatrix(this.state.matrix)
+        $.insertStandardMatrix({data:this.state.matrix})
         .then(response => {
           if (response.data === 1) {
             notification["success"]({

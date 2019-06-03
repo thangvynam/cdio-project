dc688ce780f6f973e343edc409a778e3e80f7c56
User-PC\User <hungphat11997@gmail.com>
2019-06-03 14:20:24 +0700

    add

--- src/frontend/components/matrix/matrix.js
+++ src/frontend/components/matrix/matrix.js
@@ -109,7 +109,7 @@ class Matrix extends Component {
 
     componentDidMount() {
         
-        if (this.props.isLoadEditMatrix === "false" && this.props.subjectList.length > 0) {
+        if (this.props.subjectList.length > 0) {
             this.setState({ isLoading: true })
             this.props.updateIsLoadEditMatrix("true");
             let subjectListId = [];
@@ -425,6 +425,12 @@ class Matrix extends Component {
                             columns={this.createColumn(this.props.dataMatrix)}
                             dataSource={this.createData(this.props.dataMatrix)}
                             scroll={{ x: 1500 }}
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 5,
+                            }}
                         />
                     </div>
                 }

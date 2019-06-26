7450ece896542c3a78d8abf0eb8531691299e73d
User-PC\User <hungphat11997@gmail.com>
2019-06-26 14:36:43 +0700

    fix4

--- src/frontend/Layout4/Component/Table/CDRTableItem.js
+++ src/frontend/Layout4/Component/Table/CDRTableItem.js
@@ -255,7 +255,7 @@ class CDRTableItem extends Component {
                       onClick={() => this.save(form, record.key)}
                       style={{ marginRight: 8 }}
                     >
-                      Save
+                      Lưu
                       </a>
                   )}
                 </EditableContext.Consumer>
@@ -263,7 +263,7 @@ class CDRTableItem extends Component {
                   title="Hủy bỏ?"
                   onConfirm={() => this.cancel(record.key)}
                 >
-                  <a href="#a">Cancel</a>
+                  <a href="#a">Hủy</a>
                 </Popconfirm>
               </span>
             ) : (

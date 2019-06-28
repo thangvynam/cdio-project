15128309d997eb806b3760437f1a4de3979f7ac7
namtv3 <namtv3@vng.com.vn>
2019-06-28 18:20:14 +0700

    Add config file

--- public/conf/env.js
+++ public/conf/env.js
@@ -1,5 +1,5 @@
 window.env= {
-     REACT_APP_API_PREFIX: 'http://172.29.64.132:3001',
+     REACT_APP_API_PREFIX: 'https://172.29.64.132:3001',
     //REACT_APP_API_PREFIX: 'http://localhost:3001',
     REACT_APP_API_TIMEOUT_FRAME: 5000,
 }
